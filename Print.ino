14e453245cbb0ffc7646b4c267d925be672f612c
MatthewFranke <Matthew.franke@yahoo.com>
2018-07-11 11:38:17 -0400

    Add files via upload

---
+++ Print.ino
@@ -0,0 +1,15 @@
+void setup() {
+ int my_array[5] = {1,10,100,1000,10000};
+ int i;
+ Serial.begin(9600);
+ 
+ for (i = 0; i < 5; i++)
+ {
+  Serial.println(my_array[i]);
+ }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
